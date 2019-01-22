18aa75821bfd9d58652aa954e070040d68a67ccc
clemahieu <clemahieu@gmail.com>
2019-01-22 16:22:26 +0700

    Adding online_weight table to trend online weight count.

--- nano/core_test/block_store.cpp
+++ nano/core_test/block_store.cpp
@@ -1417,3 +1417,23 @@ TEST (block_store, upgrade_sideband_epoch)
 	ASSERT_EQ (nano::process_result::progress, ledger.process (transaction, block2).code);
 	ASSERT_EQ (nano::epoch::epoch_1, store.block_version (transaction, block2.hash ()));
 }
+
+TEST (block_store, online_weight)
+{
+	nano::logging logging;
+	bool error (false);
+	nano::mdb_store store (error, logging, nano::unique_path ());
+	ASSERT_FALSE (error);
+	auto transaction (store.tx_begin (true));
+	ASSERT_EQ (0, store.online_weight_count (transaction));
+	ASSERT_EQ (store.online_weight_end (), store.online_weight_begin (transaction));
+	store.online_weight_put (transaction, 1, 2);
+	ASSERT_EQ (1, store.online_weight_count (transaction));
+	auto item (store.online_weight_begin (transaction));
+	ASSERT_NE (store.online_weight_end (), item);
+	ASSERT_EQ (1, item->first);
+	ASSERT_EQ (2, item->second.number ());
+	store.online_weight_del (transaction, 1);
+	ASSERT_EQ (0, store.online_weight_count (transaction));
+	ASSERT_EQ (store.online_weight_end (), store.online_weight_begin (transaction));
+}
